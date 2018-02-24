bb7795cbc454923efdb8f376b209468508d9cdd4
Bala <balkripra.1996@gmail.com>
2018-02-24 19:37:28 +0530

    Test

--- src/helper/shellhelperclient.cpp
+++ src/helper/shellhelperclient.cpp
@@ -25,8 +25,8 @@
 #include <QtGui/QWindow>
 
 #include <LiriWaylandClient/Surface>
-//#include <LiriWaylandClient/private/registry_p.h>
-#include <LiriWaylandClient/private/surface_p.h>
+#include </usr/include/LiriWaylandClient/0.9.0.1/LiriWaylandClient/private/registry_p.h>
+#include </usr/include/LiriWaylandClient/0.9.0.1/LiriWaylandClient/private/surface_p.h>
 
 #include "shellhelperclient.h"
 
